be4bdb394e35793252f30a910437c5b93ed80c6d
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-15 00:51:18 +0100

    uzupełnienie metod sortuj i wyswietl

--- MergeSort/Klasa.h
+++ MergeSort/Klasa.h
@@ -7,12 +7,23 @@ class SortowaniePrzezScalanie {
 void SortowaniePrzezScalanie::scal(int tablica[], int lewy, int srodek, int prawy)
 {
 
+
 }
 void SortowaniePrzezScalanie::sortuj(int tablica[], int lewy, int prawy)
 {
+    if (lewy < prawy)
+    {
+        int srodek = (lewy + prawy) / 2;
+        sortuj(tablica, lewy, srodek);
+        sortuj(tablica, srodek + 1, prawy);
+        scal(tablica, lewy, srodek, prawy);
+    }
 
 }
 void SortowaniePrzezScalanie::wyswietl(int tablica[], int rozmiar)
 {
+    for (int i = 0; i < rozmiar; i++)
+        std::cout << tablica[i] << " ";
+    std::cout << std::endl;
 
 }
