6c974b1fe200ebd247afd98703668fb36b3a0b9a
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-15 00:42:12 +0100

    Metody

---
+++ MergeSort/Klasa.h
@@ -0,0 +1,18 @@
+class SortowaniePrzezScalanie {
+	public:
+	void scal(int tablica[], int lewy, int srodek, int prawy);
+	void sortuj(int tablica[], int lewy, int prawy);
+	void wyswietl(int tablica[], int rozmiar);
+};
+void SortowaniePrzezScalanie::scal(int tablica[], int lewy, int srodek, int prawy)
+{
+
+}
+void SortowaniePrzezScalanie::sortuj(int tablica[], int lewy, int prawy)
+{
+
+}
+void SortowaniePrzezScalanie::wyswietl(int tablica[], int rozmiar)
+{
+
+}
