ac3e9d887d095e295c8da5e5b17d7863a1746864
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-22 00:17:25 +0100

    Test sprawdza, czy algorytm potrafi posortować tablicę, która jest posortowana w odwrotnej kolejnośc

--- Merge/test.cpp
+++ Merge/test.cpp
@@ -9,14 +9,26 @@ TEST(SortowaniePrzezScalanieTest, TablicaPosortowanaRosnaco)
    SortowaniePrzezScalanie sort;
     int tablica[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int kopiaTablicy[10];
-    copy(std::begin(tablica),end(tablica),begin(kopiaTablicy));
+    copy(begin(tablica),end(tablica),begin(kopiaTablicy));
     sort.sortuj(tablica, 0, 9);
     for (int i = 0; i < 10; i++)
     {
         ASSERT_EQ(tablica[i], kopiaTablicy[i]);
     }
 }
-
+TEST(SortowaniePrzezScalanieTest,TablicyPosortowanaMalejaco)
+{
+    SortowaniePrzezScalanie sort;
+     int tablica[11];
+    for (int i = 0; i < 11; ++i) {
+        tablica[i] = 10 - i;
+    }
+    sort.sortuj(tablica, 0, 10);
+    ASSERT_TRUE(std::is_sorted(tablica, tablica + 11));
+    for (int i = 0; i < 11; ++i) {
+        ASSERT_EQ(tablica[i], i);
+    }
+}
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
