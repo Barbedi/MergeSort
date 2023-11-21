e2faec0f683a7f72ef31f2c21e358f966f495ee7
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-21 23:59:18 +0100

    Test sprawdza, czy algorytm zachowuje tablicę niezmienioną,

--- Merge/test.cpp
+++ Merge/test.cpp
@@ -2,9 +2,20 @@
 #include "gtest/gtest.h"
 #include "Klasa.h"
 
+using namespace std;
 
-
-
+TEST(SortowaniePrzezScalanieTest, TablicaPosortowanaRosnaco)
+{
+   SortowaniePrzezScalanie sort;
+    int tablica[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int kopiaTablicy[10];
+    copy(std::begin(tablica),end(tablica),begin(kopiaTablicy));
+    sort.sortuj(tablica, 0, 9);
+    for (int i = 0; i < 10; i++)
+    {
+        ASSERT_EQ(tablica[i], kopiaTablicy[i]);
+    }
+}
 
 int main(int argc, char** argv)
 {
